ad56e2f84d236df4f61accf2bf6157ac910bd98f
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2016-07-05 11:30:21 +0200

    remove unused exposed method

--- lib/middleman-crawler/extension.rb
+++ lib/middleman-crawler/extension.rb
@@ -4,8 +4,6 @@ require 'middleman-core'
 # Extension namespace
 class Crawler < ::Middleman::Extension
 
-  expose_to_template :crawler
-
   def initialize(app, options_hash={}, &block)
     # Call super to build options from the options_hash
     super
